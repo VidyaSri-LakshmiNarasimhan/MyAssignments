728693a36136c8c4c10fd3108279e9bb9e80b197
VidyaSri-LakshmiNarasimhan <147947957+VidyaSri-LakshmiNarasimhan@users.noreply.github.com>
2023-10-15 01:57:36 +0530

    Add files via upload

---
+++ day1/Browser.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Browser {
+	public static void main(String[] args) {
+		float browserVersion = 1.0f;
+		String browserName = "Google Chrome";
+		boolean pageLoaded = true;
+		int noOfBrowser= 1;
+		char browserLogo = 'G';
+		System.out.println("The browser version is " + browserVersion);
+		System.out.println("The browser Name is " + browserName);
+		System.out.println("Is the page loaded" + pageLoaded);
+		System.out.println(noOfBrowser + " is open");
+		System.out.println("The browser Logo is " + browserLogo);
+	}
+
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class FindNumber {
+public static void main(String[] args) {
+	int variable = 45;
+	if(variable>0) {
+		System.out.println("The number is positive");
+	}
+	else if(variable <0) {
+		System.out.println("The number is negative");
+	}
+}
+}
